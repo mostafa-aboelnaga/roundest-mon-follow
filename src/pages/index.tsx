e3ba34660dcb90ba00add8295c67efb10f082f81
Mostafa Aboelnaga <66335224+mostafa-aboelnaga@users.noreply.github.com>
2022-09-28 02:50:06 +0200

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+  return (
+    <div className="text-center text-2xl text-red-200">
+      Hello world
+    </div>
+  )
+};
+
+export default Home;
+
+type TechnologyCardProps = {
+  name: string;
+  description: string;
+  documentation: string;
+};
+
+const TechnologyCard = ({
+  name,
+  description,
+  documentation,
+}: TechnologyCardProps) => {
+  return (
+    <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
+      <h2 className="text-lg text-gray-700">{name}</h2>
+      <p className="text-sm text-gray-600">{description}</p>
+      <a
+        className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
+        href={documentation}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Documentation
+      </a>
+    </section>
+  );
+};
