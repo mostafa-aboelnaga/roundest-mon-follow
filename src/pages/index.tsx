1fe4b3c228c2ce41560dc702f03e70486a65262a
Mostafa Aboelnaga <66335224+mostafa-aboelnaga@users.noreply.github.com>
2022-09-28 03:02:15 +0200

    Basic UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,37 +4,16 @@ import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
   return (
-    <div className="text-center text-2xl text-red-200">
-      Hello world
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon is rounder?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex items-center justify-between max-w-2xl">
+        <div className="w-16 h-16 bg-red-200" />
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200" />
+      </div>
     </div>
-  )
+  );
 };
 
 export default Home;
-
-type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
-};
-
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <a
-        className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </a>
-    </section>
-  );
-};
