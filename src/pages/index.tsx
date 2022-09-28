8a7a47f15d2753d639782d707b7727fd40f4339e
Mostafa Aboelnaga <66335224+mostafa-aboelnaga@users.noreply.github.com>
2022-09-28 03:52:14 +0200

    backend with pokemon data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,45 @@
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useMemo, useState } from "react";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const {data, isLoading} = trpc.useQuery(['example.hello', {text: "Mostafa"}])
-  if (isLoading) return <div>Loading...</div>
-  if (data) return <div>{data.greeting}</div>
+  const [ids, updateIds] = useState(() => getOptionsForVote());
+
+  const [first, second] = ids;
+
+  const firstPokemon = trpc.useQuery(["pokemons.get-pokemon", { id: first! }]);
+  const secondPokemon = trpc.useQuery([
+    "pokemons.get-pokemon",
+    { id: second! },
+  ]);
+
+  if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex items-center justify-between max-w-2xl">
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-64 h-64 flex flex-col justify-center items-center">
+          <img
+            className="w-full"
+            src={firstPokemon.data?.sprites.front_default!}
+            alt=" "
+          />
+          <div className="text-xl capitalize mt-[-2rem]">{firstPokemon.data?.name}</div>
+        </div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-64 h-64 flex flex-col justify-center items-center">
+          <img
+            className="w-full"
+            src={secondPokemon.data?.sprites.front_default!}
+            alt=" "
+          />
+          <div className="text-xl capitalize mt-[-2rem]">{secondPokemon.data?.name}</div>
+        </div>
+        <div className="p-3" />
       </div>
     </div>
   );
--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,12 +2,12 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
+import { pokemonsRouter } from "./pokemons";
 import { protectedExampleRouter } from "./protected-example-router";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
+  .merge("pokemons.", pokemonsRouter)
   .merge("auth.", protectedExampleRouter);
 
 // export type definition of API
